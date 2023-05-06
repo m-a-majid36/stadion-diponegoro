372e0a4798d460732b823731479c7b3635f0a318
m-a-majid36 <muhammadabdulmajid36@Gmail.com>
2023-05-06 13:21:36 +0700

    Watermark Kuitansi

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Pembayaran;
-use App\Models\Pembukuan;
-use App\Models\Penyewa;
 use App\Models\Ruko;
-use Barryvdh\DomPDF\Facade\Pdf;
+use App\Models\Penyewa;
+use App\Models\Pembukuan;
+use App\Models\Pembayaran;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Storage;
-use Whoops\Run;
 
 class PembayaranController extends Controller
 {
@@ -259,6 +258,18 @@ class PembayaranController extends Controller
         $batas = date('d', strtotime($pembayaran->deadline)) . ' ' . $bulandeadline . ' ' . date('Y', strtotime($pembayaran->deadline));
 
         $pdf = Pdf::loadView('menu.pembayaran.print', compact('pembayaran', 'tanggal', 'terbilang', 'batas'))->setPaper('a4', 'potrait');
+        $pdf->output();
+        $canvas = $pdf->getDomPDF()->getCanvas();
+
+        $height = $canvas->get_height();
+        $width = $canvas->get_width();
+
+        $canvas->set_opacity(.2,"Multiply");
+
+        $canvas->set_opacity(.2);
+
+        $canvas->page_text($width/7, $height/5, 'STADION DIPONEGORO', null, 38, array(0,1,0),3,3,-20);
+        $canvas->page_text($width/3, $height/5, 'SEMARANG', null, 45, array(0,1,0),3,3,-20);
 
         return $pdf->download("Kuitansi " . $pembayaran->kode . " " . date('d', strtotime($pembayaran->created_at)) . '-' . $bulan . '-' . date('Y', strtotime($pembayaran->created_at)) . ".pdf");
     }
